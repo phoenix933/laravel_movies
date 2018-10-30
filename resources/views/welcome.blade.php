1ad32bc29ac0fb3aa0c76a084717f9337e2fe7a5
crashdev226 <midashand44@gmail.com>
2018-10-30 14:16:20 +0330

    Some changes in HTML in order to needs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,25 +28,16 @@
     				<a href="#">
     					<h3 class="movie-title">Man of Steel</h3>
     				</a>
-    				<div class="imax-logo"></div>
     			</div>
     			<div class="movie-info">
     				<div class="info-section">
-    					<label>Date & Time</label>
+    					<label>Release Date & Time</label>
     					<span>Sun 8 Sept - 10:00PM</span>
     				</div><!--date,time-->
     				<div class="info-section">
-    					<label>Screen</label>
-    					<span>03</span>
-    				</div><!--screen-->
-    				<div class="info-section">
-    					<label>Row</label>
-    					<span>F</span>
-    				</div><!--row-->
-    				<div class="info-section">
-    					<label>Seat</label>
-    					<span>21,22</span>
-    				</div><!--seat-->
+    					<label>Ticket Price</label>
+    					<span>50 $</span>
+    				</div><!--ticket price-->
     			</div>
     		</div><!--movie-content-->
     	</div><!--movie-card-->
@@ -65,25 +56,16 @@
     				<a href="#">
     					<h3 class="movie-title">Baby Driver</h3>
     				</a>
-    				<div class="imax-logo"></div>
     			</div>
     			<div class="movie-info">
     				<div class="info-section">
-    					<label>Date & Time</label>
+    					<label>Release Date & Time</label>
     					<span>Tue 4 July - 05:00PM</span>
     				</div><!--date,time-->
     				<div class="info-section">
-    					<label>Screen</label>
-    					<span>01</span>
-    				</div><!--screen-->
-    				<div class="info-section">
-    					<label>Row</label>
-    					<span>H</span>
-    				</div><!--row-->
-    				<div class="info-section">
-    					<label>Seat</label>
-    					<span>15</span>
-    				</div><!--seat-->
+    					<label>Ticket Price</label>
+    					<span>50 $</span>
+    				</div><!--ticket price-->
     			</div>
     		</div><!--movie-content-->
     	</div><!--movie-card-->
@@ -102,25 +84,16 @@
     				<a href="#">
     					<h3 class="movie-title">The Dark Tower</h3>
     				</a>
-    				<div class="imax-logo"></div>
     			</div>
     			<div class="movie-info">
     				<div class="info-section">
-    					<label>Date & Time</label>
+    					<label>Release Date & Time</label>
     					<span>Wed 16 Aug - 07:00PM</span>
     				</div><!--date,time-->
     				<div class="info-section">
-    					<label>Screen</label>
-    					<span>06</span>
-    				</div><!--screen-->
-    				<div class="info-section">
-    					<label>Row</label>
-    					<span>C</span>
-    				</div><!--row-->
-    				<div class="info-section">
-    					<label>Seat</label>
-    					<span>18</span>
-    				</div><!--seat-->
+    					<label>Ticket Price</label>
+    					<span>50 $</span>
+    				</div><!--ticket price-->
     			</div>
     		</div><!--movie-content-->
     	</div><!--movie-card-->
@@ -139,25 +112,16 @@
     				<a href="#">
     					<h3 class="movie-title">Blade Runner 2049</h3>
     				</a>
-    				<div class="imax-logo"></div>
     			</div>
     			<div class="movie-info">
     				<div class="info-section">
-    					<label>Date & Time</label>
+    					<label>Release Date & Time</label>
     					<span>Mon 16 Oct - 10:00PM</span>
     				</div><!--date,time-->
     				<div class="info-section">
-    					<label>Screen</label>
-    					<span>06</span>
-    				</div><!--screen-->
-    				<div class="info-section">
-    					<label>Row</label>
-    					<span>D</span>
-    				</div><!--row-->
-    				<div class="info-section">
-    					<label>Seat</label>
-    					<span>05,06</span>
-    				</div><!--seat-->
+    					<label>Ticket Price</label>
+    					<span>50 $</span>
+    				</div><!--ticket price-->
     			</div>
     		</div><!--movie-content-->
     	</div><!--movie-card-->
